7d6cfec4a376823dfc3088248961b8d2931d20a4
Mochammad Miftahul Huda Firmansyah <iniakunhuda@gmail.com>
2024-07-28 22:10:52 +0700

    add crud user

---
+++ users/cmd/app/helpers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (app *Application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Output(2, trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *Application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// HashPassword generates a bcrypt hash for the given password.
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// VerifyPassword verifies if the given password matches the stored hash.
+func VerifyPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
--- users/cmd/app/routes.go
+++ users/cmd/app/routes.go
@@ -4,10 +4,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func (app *application) routes() *mux.Router {
-	// Register handler functions.
+func (app *Application) Routes() *mux.Router {
 	r := mux.NewRouter()
+
 	r.HandleFunc("/api/users/", app.all).Methods("GET")
+	r.HandleFunc("/api/users/{id}", app.findByID).Methods("GET")
+	r.HandleFunc("/api/users/", app.insert).Methods("POST")
+	r.HandleFunc("/api/users/{id}", app.update).Methods("PUT")
 
 	return r
 }
